d21c424bb0084660d27ed28bd63711f1742fb623
Dahlos <seba.manriquezh@gmail.com>
2022-03-04 16:17:34 -0300

    bottom bar added, and scaffold structure set

--- app/src/main/java/com/example/componecloneui/instagram/HomeInstagramActivity.kt
+++ app/src/main/java/com/example/componecloneui/instagram/HomeInstagramActivity.kt
@@ -7,16 +7,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,12 +27,12 @@ class HomeInstagramActivity : ComponentActivity() {
         setContent {
             ComponeCloneUITheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    bottomBar = { BottomBar() },
+                    topBar = { ToolbarApp() }
                 ) {
                     Column() {
-                        ToolbarApp()
                         StoriesBar()
                     }
                 }
@@ -74,26 +72,73 @@ fun ToolbarApp() {
 
 @Preview(showBackground = true)
 @Composable
+fun BottomBar() {
+    BottomNavigation(
+        backgroundColor = colorResource(id = R.color.white),
+        contentColor = Color.Black
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround, modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterVertically)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_baseline_home_24),
+                contentDescription = "Home",
+                Modifier.align(Alignment.CenterVertically)
+            )
+            Image(
+                painter = painterResource(R.drawable.ic_baseline_search_24),
+                contentDescription = "Search",
+                Modifier.align(Alignment.CenterVertically)
+            )
+            Image(
+                painter = painterResource(R.drawable.ic_instagram_reels),
+                contentDescription = "Instagram Icon",
+                Modifier.align(Alignment.CenterVertically)
+            )
+            Image(
+                painter = painterResource(R.drawable.ic_baseline_shopping_basket_24),
+                contentDescription = "Instagram Icon",
+                Modifier.align(Alignment.CenterVertically)
+            )
+            Image(
+                painter = painterResource(R.drawable.spiderman_profile),
+                contentDescription = "Instagram Icon",
+                Modifier
+                    .padding(top = 2.dp, bottom = 2.dp)
+                    .size(28.dp)
+                    .clip(CircleShape),   // clip to the circle shape
+                contentScale = ContentScale.Crop, // crop the image if it's not a square
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
 fun StoriesBar() {
     Row(
-        horizontalArrangement = Arrangement.SpaceAround, modifier = Modifier
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
             .fillMaxWidth()
     ) {
-        AddStoryUser(Modifier.align(Alignment.CenterVertically))
-        StoryUserAvatar(Modifier.align(Alignment.CenterVertically))
-        StoryUserAvatar(Modifier.align(Alignment.CenterVertically))
-        StoryUserAvatar(Modifier.align(Alignment.CenterVertically))
-        StoryUserAvatar(Modifier.align(Alignment.CenterVertically))
+        AddStoryUser()
+        StoryUserAvatar()
+        StoryUserAvatar()
+        StoryUserAvatar()
+        StoryUserAvatar()
     }
 }
 
 @Composable
-fun AddStoryUser(modifier: Modifier) {
+fun AddStoryUser() {
     Box() {
         Image(
             painter = painterResource(R.drawable.spiderman_profile),
             contentDescription = "Instagram Icon",
-            modifier
+            Modifier
                 .padding(top = 2.dp, bottom = 2.dp)
                 .size(58.dp)
                 .clip(CircleShape)   // clip to the circle shape
@@ -117,11 +162,11 @@ fun AddStoryUser(modifier: Modifier) {
 }
 
 @Composable
-fun StoryUserAvatar(modifier: Modifier) {
+fun StoryUserAvatar() {
     Image(
         painter = painterResource(R.drawable.batman_profile),
         contentDescription = "Instagram Icon",
-        modifier
+        Modifier
             .padding(top = 2.dp, bottom = 2.dp)
             .size(58.dp)
             .clip(CircleShape)   // clip to the circle shape
